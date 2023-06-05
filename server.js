6fa7dcb8e7407d07e7591163544acf80c534b5c3
Clara V. Alarcon <claravictorialarcon@gmail.com>
2023-06-05 19:07:11 -0300

    añadimos el atributo password a User

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(routes);
 const port = process.env.SERVER_PORT;
 
-await connectionDb.sync({force: false}).then(()=>{
+await connectionDb.sync({alter: true}).then(()=>{
     app.listen(port, ()=>{
         console.log("server ok http://localhost:8080");
     })
